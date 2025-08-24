30e22e3ad4f9f3b7e60ff912f2cb4744cff91c3e
asad-mansuri <mansuriasad348@gmail.com>
2025-08-25 00:01:36 +0530

    Add student details script
    Introduced a script that defines a student object and prints its details to the console. Includes instructions for running the script using Node.js.

---
+++ Day 1/script.js
@@ -0,0 +1,15 @@
+// Define a student object
+const student = {
+  name: "Kunal Sharma",
+  age: 20,
+  course: "BTech CSE",
+  college: "SISTEC Bhopal"
+};
+
+// Print details
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("College:", student.college);
+// for running write "node script.js" in terminal and hit enter
